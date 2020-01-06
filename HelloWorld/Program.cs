639834a8343e52c6c9d24c24884cf0b92fd4f421
Márk Diószegi <walnut5588@gmail.com>
2020-01-06 04:25:59 +0100

    Added the simple Hello World program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SI1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine("Hello {0}", arg);
+            }
+        }
+    }
+}
